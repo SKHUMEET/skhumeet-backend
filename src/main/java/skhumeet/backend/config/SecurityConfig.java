2d2fcaa3026774829d61a679749ddb9a7625c51b
hangillee <skfcb10@naver.com>
2023-04-08 21:14:01 +0900

    chore: edit Spring Security configuration

--- src/main/java/skhumeet/backend/config/SecurityConfig.java
+++ src/main/java/skhumeet/backend/config/SecurityConfig.java
@@ -59,6 +59,7 @@ public class SecurityConfig {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
         configuration.addAllowedOrigin("http://localhost:3000");
+        configuration.addAllowedOrigin("http://api-skhumeet.duckdns.org");
         configuration.setAllowedMethods(List.of(
                 HttpMethod.GET.name(),
                 HttpMethod.POST.name(),
